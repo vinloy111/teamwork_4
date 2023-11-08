8a722d8a06093e519663a44779290da6780093bf
Шторк Андрей Олегович <shtork@lanit.ru>
2023-11-09 00:17:08 +0300

    TEA-35 material-ui, formik, yup

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from '../features/homePageSlice'
 import { RootState } from '../store'
+import Button from '@mui/material/Button'
+import ExampleForm from '../components/ExampleForm'
 
 function HomePageComponent() {
   const count = useSelector((state: RootState) => state.homePage.value)
@@ -11,9 +13,22 @@ function HomePageComponent() {
     <div>
       <h1>Home Page</h1>
       {/* TODO Удалить, когда начнется разработка */}
-      <div>Пример использования стора: {count}</div>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <div>Пример использования стора и material: {count}</div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => dispatch(increment())}>
+        Increment
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={() => dispatch(decrement())}>
+        Decrement
+      </Button>
+
+      <h2>Пример использования формы</h2>
+      <ExampleForm />
     </div>
   )
 }
