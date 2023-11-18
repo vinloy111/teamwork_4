19d349ceb71aebfe5dc3bdeffe2f51421e14e730
Шторк Андрей Олегович <shtork@lanit.ru>
2023-11-18 23:40:27 +0300

    TEA-23 Вёрстка страницы регистрации

--- packages/client/src/pages/login/LoginPage.tsx
+++ packages/client/src/pages/login/LoginPage.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import { Button, TextField, Container, Typography } from '@mui/material'
-import { theme } from '../../theme'
+import { Button, TextField, Container } from '@mui/material'
+import StyledHeader from '../../components/styled-header/StyledHeader'
 
 function LoginPage() {
   return (
     <Container component="main" maxWidth="xs">
       <div className="login-form">
-        <Typography
-          variant={'h3'}
-          sx={{
-            color: theme.palette.warning.main,
-            zIndex: 10,
-            backgroundColor: theme.palette.background.paper,
-            p: theme.spacing(2),
-            m: theme.spacing(2),
-            borderRadius: 5,
-          }}>
-          Авторизация
-        </Typography>
+        <StyledHeader text="Авторизация" />
         <form noValidate>
           <TextField
             variant="outlined"
