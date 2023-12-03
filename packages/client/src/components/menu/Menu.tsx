6c55a90b341e6aee620fcfb340b24fbb6b7e6bc6
ellevanova <ellevanova@gmail.com>
2023-12-03 20:38:05 +0400

    added web api: full screen with hook

--- packages/client/src/components/menu/Menu.tsx
+++ packages/client/src/components/menu/Menu.tsx
@@ -1,14 +1,25 @@
-import { AppBar, Box, MenuItem, Toolbar, Typography } from '@mui/material'
+import {
+  AppBar,
+  Box,
+  IconButton,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from '@mui/material'
 import { Link, useNavigate } from 'react-router-dom'
 import React from 'react'
 import Button from '@mui/material/Button'
 import yApiService from '../../services/y-api-service'
 import { useDispatch } from 'react-redux'
 import { clearUser } from 'features/authSlice'
+import FullscreenIcon from '@mui/icons-material/Fullscreen'
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit'
+import useFullScreen from 'hooks/useFullScreen'
 
 const Menu = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const { isFullscreen, handleFullScreen } = useFullScreen()
 
   const handleLogout = async () => {
     try {
@@ -23,35 +34,42 @@ const Menu = () => {
   return (
     <Box>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar sx={{ flexGrow: 1, direction: 'row' }}>
           <MenuItem component={Link} to={'/'}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               Home
             </Typography>
           </MenuItem>
           <MenuItem component={Link} to={'/game'}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               Game
             </Typography>
           </MenuItem>
           <MenuItem component={Link} to={'/forum'}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               Forum
             </Typography>
           </MenuItem>
           <MenuItem component={Link} to={'/leaderboard'}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               Leaderboard
             </Typography>
           </MenuItem>
           <MenuItem component={Link} to={'/settings'}>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" component="div">
               Settings
             </Typography>
           </MenuItem>
           <Button color="inherit" onClick={handleLogout}>
             Logout
           </Button>
+          <Box sx={{ flexGrow: 1 }}></Box>
+          <IconButton
+            onClick={handleFullScreen}
+            aria-label="delete"
+            size="large">
+            {isFullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
---
+++ packages/client/src/hooks/useFullScreen.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+
+const useFullScreen = (): {
+  isFullscreen: boolean
+  handleFullScreen: () => void
+} => {
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  useEffect(() => {
+    function onFullscreenChange() {
+      setIsFullscreen(Boolean(document.fullscreenElement))
+    }
+
+    document.addEventListener('fullscreenchange', onFullscreenChange)
+
+    return () =>
+      document.removeEventListener('fullscreenchange', onFullscreenChange)
+  }, [])
+  useEffect(() => {
+    function onEnterF11(event: KeyboardEvent) {
+      if (event.key === 'F11') {
+        event.preventDefault()
+        handleFullScreen()
+      }
+    }
+
+    document.addEventListener('keydown', onEnterF11, false)
+
+    return () => document.removeEventListener('keydown', onEnterF11)
+  }, [])
+  const handleFullScreen = async () => {
+    if (!isFullscreen) {
+      await document.documentElement.requestFullscreen()
+    } else if (document.exitFullscreen || isFullscreen) {
+      await document.exitFullscreen()
+    }
+  }
+  return { isFullscreen, handleFullScreen }
+}
+
+export default useFullScreen
