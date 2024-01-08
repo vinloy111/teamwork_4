32ed301c449aa8396a531dfefae464117ae8a4ba
Шторк Андрей Олегович <shtork@lanit.ru>
2024-01-08 12:55:07 +0300

    TEA-52 прод сборка для проекта

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -4,24 +4,26 @@ const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
   process.env
 
 export const createClientAndConnect = async (): Promise<Client | null> => {
-  try {
-    const client = new Client({
-      user: POSTGRES_USER,
-      host: 'localhost',
-      database: POSTGRES_DB,
-      password: POSTGRES_PASSWORD,
-      port: Number(POSTGRES_PORT),
-    })
+  const client = new Client({
+    user: POSTGRES_USER,
+    host: 'postgres',
+    database: POSTGRES_DB,
+    password: POSTGRES_PASSWORD,
+    port: Number(POSTGRES_PORT),
+  })
 
-    await client.connect()
-
-    const res = await client.query('SELECT NOW()')
-    console.log('  ➜ 🎸 Connected to the database at:', res?.rows?.[0].now)
-    client.end()
-
-    return client
-  } catch (e) {
-    console.error(e)
+  let attempts = 5
+  while (attempts > 0) {
+    try {
+      await client.connect()
+      console.log('Connected to the database')
+      return client
+    } catch (e) {
+      console.error(e)
+      attempts--
+      console.log(`Retrying to connect, attempts left: ${attempts}`)
+      await new Promise(resolve => setTimeout(resolve, 5000)) // задержка 5 секунд
+    }
   }
 
   return null
